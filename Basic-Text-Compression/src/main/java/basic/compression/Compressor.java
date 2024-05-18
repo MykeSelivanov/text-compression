4353c071ad43988923cce1a6c4827685e613f3aa
MykeSelivanov <michael777selivanov@gmail.com>
2024-05-17 23:02:29 -0500

    compression

---
+++ Basic-Text-Compression/src/main/java/basic/compression/Compressor.java
@@ -0,0 +1,11 @@
+package basic.compression;
+
+public class Compressor {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public void compress() {
+        System.out.println("test");
+    }
+}
